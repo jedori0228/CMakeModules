fae7f7cbf0a15ad4df0b56e1ac0b39ef25b688b0
Luke Pickering <luketpickering@gmail.com>
2022-06-22 18:13:49 -0400

    If a specific GENIE version has already been found, FindGENIE can now use that rather than just getting confused.

--- FindGENIE.cmake
+++ FindGENIE.cmake
@@ -26,44 +26,63 @@ set(GENIE2_XSECEMPMEC_ENABLED FALSE)
 set(GENIE3_API_ENABLED FALSE)
 set(GENIE3_XSECMEC_ENABLED FALSE)
 
-set(GENIE2_FOUND FALSE)
-set(GENIE3_FOUND FALSE)
-
-include(CMessage)
-include(ParseConfigApps)
-
-find_package(GENIEVersion)
-if(NOT GENIEVersion_FOUND)
-  SET(GENIE_FOUND FALSE)
-  return()
-endif()
-
-if(GENIE_VERSION VERSION_GREATER 2.99.99)
-	find_package(GENIE3)
-  if(NOT GENIE3_FOUND)
-    SET(GENIE_FOUND FALSE)
-    return()
-  else()
-    SET(GENIE_FOUND TRUE)
-  endif()
-
-  add_library(GENIE::All INTERFACE IMPORTED)
-  set_target_properties(GENIE::All PROPERTIES
-      INTERFACE_LINK_LIBRARIES GENIE3::All
-  )
-else()
-	find_package(GENIE2)
-  if(NOT GENIE2_FOUND)
-    SET(GENIE_FOUND FALSE)
-    return()
-  else()
-    SET(GENIE_FOUND TRUE)
-  endif()
-
+if(NOT DEFINED GENIE2_FOUND)
+  set(GENIE2_FOUND FALSE)
+elseif(GENIE2_FOUND) #We might have already found the version-specifc one
   add_library(GENIE::All INTERFACE IMPORTED)
   set_target_properties(GENIE::All PROPERTIES
       INTERFACE_LINK_LIBRARIES GENIE2::All
   )
+  SET(GENIE_FOUND TRUE)
+endif()
+
+if(NOT DEFINED GENIE3_FOUND)
+  set(GENIE3_FOUND FALSE)
+elseif(GENIE3_FOUND) #We might have already found the version-specifc one
+  add_library(GENIE::All INTERFACE IMPORTED)
+  set_target_properties(GENIE::All PROPERTIES
+      INTERFACE_LINK_LIBRARIES GENIE3::All
+  )
+  SET(GENIE_FOUND TRUE)
+endif()
+
+if(NOT GENIE_FOUND)
+  include(CMessage)
+  include(ParseConfigApps)
+
+  find_package(GENIEVersion)
+  if(NOT GENIEVersion_FOUND)
+    SET(GENIE_FOUND FALSE)
+    return()
+  endif()
+
+  if(GENIE_VERSION VERSION_GREATER 2.99.99)
+  	find_package(GENIE3)
+    if(NOT GENIE3_FOUND)
+      SET(GENIE_FOUND FALSE)
+      return()
+    else()
+      SET(GENIE_FOUND TRUE)
+    endif()
+
+    add_library(GENIE::All INTERFACE IMPORTED)
+    set_target_properties(GENIE::All PROPERTIES
+        INTERFACE_LINK_LIBRARIES GENIE3::All
+    )
+  else()
+  	find_package(GENIE2)
+    if(NOT GENIE2_FOUND)
+      SET(GENIE_FOUND FALSE)
+      return()
+    else()
+      SET(GENIE_FOUND TRUE)
+    endif()
+
+    add_library(GENIE::All INTERFACE IMPORTED)
+    set_target_properties(GENIE::All PROPERTIES
+        INTERFACE_LINK_LIBRARIES GENIE2::All
+    )
+  endif()
 endif()
 
 if(GENIE_FOUND)
