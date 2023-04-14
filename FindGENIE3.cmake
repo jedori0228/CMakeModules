29bfb2b5d371a1b6fe172dfbce305c7a1aa88c13
Luke Pickering <luketpickering@gmail.com>
2023-04-14 10:36:30 +0100

    Fixes bug where linking to GENIE3 without GENIEReWeight built broken downstream dependencies

--- FindGENIE3.cmake
+++ FindGENIE3.cmake
@@ -77,24 +77,40 @@ if(NOT TARGET GENIE3::All)
     LIST(APPEND GENIE_DEFINES -DGENIE_ENABLED -DGENIE_VERSION=${GENIE_SINGLE_VERSION})
 
     set(GENIEReWeight_ENABLED TRUE)
+    # Remove the libraries from GENIE libs, if we think we can find them, 
+    #   we will append them again in a moment
     foreach(RWLIBNAME GRwClc GRwFwk GRwIO)
       LIST(REMOVE_ITEM GENIE_LIBS ${RWLIBNAME})
-      if(EXISTS ${GENIE_RW_LIB_DIR}/lib${RWLIBNAME}.so)
-        LIST(APPEND GENIE_LIBS ${RWLIBNAME})
-      else()
-        cmessage(WARNING "Failed to find expected reweight library: ${GENIE_RW_LIB_DIR}/lib${RWLIBNAME}.so disabling GENIE3 reweight.")
-        set(GENIEReWeight_ENABLED FALSE)
-      endif()
     endforeach()
 
-    if(EXISTS ${GENIE_RW_INC_DIR}/RwCalculators/GReWeightXSecMEC.h)
-      SET(GENIE3_XSECMEC_ENABLED TRUE)
-    endif()
+    if( (GENIE_RW_INC_DIR  STREQUAL "GENIE_RW_INC_DIR-NOTFOUND") 
+        OR (GENIE_RW_LIB_DIR  STREQUAL "GENIE_RW_LIB_DIR-NOTFOUND") )
+      set(GENIEReWeight_ENABLED FALSE)
+    else()
+      foreach(RWLIBNAME GRwClc GRwFwk GRwIO)
+        if(EXISTS ${GENIE_RW_LIB_DIR}/lib${RWLIBNAME}.so)
+          LIST(APPEND GENIE_LIBS ${RWLIBNAME})
+        else()
+          cmessage(WARNING "Failed to find expected reweight library: ${GENIE_RW_LIB_DIR}/lib${RWLIBNAME}.so disabling GENIE3 reweight.")
+          set(GENIEReWeight_ENABLED FALSE)
+        endif()
+      endforeach()
 
+      if(EXISTS ${GENIE_RW_INC_DIR}/RwCalculators/GReWeightXSecMEC.h)
+        SET(GENIE3_XSECMEC_ENABLED TRUE)
+      endif()
+    endif()
 
     #duplicate because CMake gets its grubby mitts on repeated -Wl,--start-group options
     SET(GENIE_LIBS "-Wl,--no-as-needed;${GENIE_LIBS};${GENIE_LIBS};-Wl,--as-needed")
     SET(GENIE_DEP_LIBS EGPythia6 Pythia6::Pythia6 LHAPDF::LHAPDF log4cpp::log4cpp LibXml2::LibXml2 GSL::gsl)
+
+    LIST(APPEND GENIE_ALL_INC_DIRS ${GENIE_INC_DIR})
+    LIST(APPEND GENIE_ALL_LIB_DIRS ${GENIE_LIB_DIR})
+    if(GENIEReWeight_ENABLED)
+      LIST(APPEND GENIE_ALL_LIB_DIRS ${GENIE_RW_LIB_DIR})
+      LIST(APPEND GENIE_ALL_INC_DIRS ${${GENIE_RW_INC_DIR}})
+    endif()
 
     cmessage(STATUS "GENIE 3 (Version: ${GENIE_VERSION})")
     cmessage(STATUS "                GENIE: ${GENIE}")
@@ -102,20 +118,15 @@ if(NOT TARGET GENIE3::All)
     cmessage(STATUS "              OPTIONS: GENIEReWeight: ${GENIEReWeight_ENABLED}, XSecMECReWeight: ${GENIE3_XSECMEC_ENABLED}")
     cmessage(STATUS " GENIE_SINGLE_VERSION: ${GENIE_SINGLE_VERSION}")
     cmessage(STATUS "        GENIE DEFINES: ${GENIE_DEFINES}")
-    cmessage(STATUS "       GENIE INC_DIRS: ${GENIE_INC_DIR} ${GENIE_RW_INC_DIR}")
-    cmessage(STATUS "       GENIE LIB_DIRS: ${GENIE_LIB_DIR}")
-    cmessage(STATUS "    GENIE RW_LIB_DIRS: ${GENIE_RW_LIB_DIR}")
+    cmessage(STATUS "       GENIE INC_DIRS: ${GENIE_ALL_INC_DIRS}")
+    cmessage(STATUS "       GENIE LIB_DIRS: ${GENIE_ALL_LIB_DIRS}")
     cmessage(STATUS "           GENIE LIBS: ${GENIE_LIBS}")
     cmessage(STATUS "            DEPS LIBS: ${GENIE_DEP_LIBS}")
 
-    LIST(APPEND GENIE_ALL_LIB_DIRS ${GENIE_LIB_DIR})
-    if(GENIEReWeight_ENABLED)
-      LIST(APPEND GENIE_ALL_LIB_DIRS ${GENIE_RW_LIB_DIR})
-    endif()
 
     add_library(GENIE3::All INTERFACE IMPORTED)
     set_target_properties(GENIE3::All PROPERTIES
-      INTERFACE_INCLUDE_DIRECTORIES "${GENIE_INC_DIR};${GENIE_RW_INC_DIR}"
+      INTERFACE_INCLUDE_DIRECTORIES "${GENIE_ALL_INC_DIRS}"
       INTERFACE_COMPILE_OPTIONS "${GENIE_DEFINES}"
       INTERFACE_LINK_DIRECTORIES "${GENIE_ALL_LIB_DIRS}"
       INTERFACE_LINK_LIBRARIES "${GENIE_LIBS};${GENIE_DEP_LIBS}"
